f66c836b2e25e07611b060a11e2b48c3bd57fb72
Hendrik Lux <35100899+ArdentEmpiricist@users.noreply.github.com>
2024-07-04 13:41:41 +0200

    Clippy lints

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,10 @@ fn main() -> std::io::Result<()> {
             let entry = entry.expect("error unwrapping entry");
             let file_path = entry.path();
             //check if path is file, is a pdf file and if the filename does not start with "20" (as this would indicate it already got renamed)
-            if file_path.is_file() && file_path.extension().and_then(OsStr::to_str) == Some("pdf") && entry.file_name().to_str().unwrap().starts_with("20") == false {
+            if file_path.is_file() && file_path.extension().and_then(OsStr::to_str) == Some("pdf") && !entry.file_name().to_str().unwrap().starts_with("20") {
                 println!("Renamed {:?} to {:?}", entry.file_name(), path.file_name().unwrap());
                     rename(&file_path)?;
-            } else if file_path.is_file() && file_path.extension().and_then(OsStr::to_str) == Some("pdf") && entry.file_name().to_str().unwrap().starts_with("20") == true {
+            } else if file_path.is_file() && file_path.extension().and_then(OsStr::to_str) == Some("pdf") && entry.file_name().to_str().unwrap().starts_with("20") {
                 println!("File {:?} ignored as it seems to have been renamed already.", entry.file_name());
             }
         }
@@ -40,7 +40,7 @@ pub fn rename(path: &Path) -> std::io::Result<()> {
     let mut new_path = PathBuf::new();
 
     //add parent path to new path
-    new_path.push(&path.parent().unwrap());
+    new_path.push(path.parent().unwrap());
     
     //read pdf file
     let bytes = std::fs::read(path).unwrap();
@@ -57,16 +57,16 @@ pub fn rename(path: &Path) -> std::io::Result<()> {
 
     //println!("{:?}", date);
 
-    let vec_date: Vec<&str> = date.split(".").collect();
+    let vec_date: Vec<&str> = date.split('.').collect();
 
     //organise date to yyyy_mm_dd
     let mut date_ordertype_name: String = String::new();
     date_ordertype_name.push_str(vec_date[2]);
-    date_ordertype_name.push_str("_");
+    date_ordertype_name.push('_');
     date_ordertype_name.push_str(vec_date[1]);
-    date_ordertype_name.push_str("_");
+    date_ordertype_name.push('_');
     date_ordertype_name.push_str(vec_date[0]);
-    date_ordertype_name.push_str("_");
+    date_ordertype_name.push('_');
 
     //println!("date: {:?}", date_ordertype_name);
 
@@ -95,7 +95,7 @@ pub fn rename(path: &Path) -> std::io::Result<()> {
     //finalize new filename as date_ordertype_name.pdf
     date_ordertype_name.push_str(&order_type);
 
-    date_ordertype_name.push_str("_");
+    date_ordertype_name.push('_');
 
     date_ordertype_name.push_str(&name);
 
